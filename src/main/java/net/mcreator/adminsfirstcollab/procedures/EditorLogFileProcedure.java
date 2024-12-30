51382fa62e001ab2a351d692cc06c5cc6fa07cc0
Aefseaff <aefse@DESKTOP-B0E04SD.attlocal.net>
2024-12-29 20:56:50 -0800

    First Version

---
+++ src/main/java/net/mcreator/adminsfirstcollab/procedures/EditorLogFileProcedure.java
@@ -0,0 +1,9 @@
+package net.mcreator.adminsfirstcollab.procedures;
+
+import net.mcreator.adminsfirstcollab.AdminsfirstcollabMod;
+
+public class EditorLogFileProcedure {
+	public static void execute() {
+		AdminsfirstcollabMod.LOGGER.info("This is the editor log file" + "Last Editor: Aefseaff" + "Last Edited: 12/29/2024 at 8:56PM" + "Version: Beta 0.1.0" + "Changelog: First Version");
+	}
+}
